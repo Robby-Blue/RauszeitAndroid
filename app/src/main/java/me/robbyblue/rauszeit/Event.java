31e1a2f516d0e11d0ce84b5e1684335d39bceaba
Robby_Blue <69482496+Robby-Blue@users.noreply.github.com>
2024-02-25 20:26:55 +0100

    show more event data

--- app/src/main/java/me/robbyblue/rauszeit/Event.java
+++ app/src/main/java/me/robbyblue/rauszeit/Event.java
@@ -9,6 +9,10 @@ public class Event {
 
     private final String name;
     private final Spanned description;
+    private final String date;
+    private final String time;
+    private final String locationName;
+    private final String locationLink;
     // add more here, like date and time
 
     public Event(Element eventData) {
@@ -17,13 +21,27 @@ public class Event {
         Element nameElement = article.getElementsByTag("h1").get(0);
         this.name = nameElement.text();
 
-        // one br has style clear:both, after this its description
         Element entry = eventData.getElementsByClass("entry-content").get(0);
+
+        // first child is a p which includes the time and date
+        // date is an untagged text node and time in an i
+        Element timeElement = entry.child(0);
+        this.date = timeElement.textNodes().get(1).text().trim();
+        this.time = timeElement.getElementsByTag("i").get(0).text().trim();
+
+        // get the location, its the second p in the entry
+        // theres an a with the href and and name as text
+        Element locationElement = entry.child(1).getElementsByTag("a").get(0);
+        this.locationName = locationElement.text();
+        this.locationLink = locationElement.attr("href");
+
+        // get description
+        // one br has style clear:both, after this its description
         Element clearElement = entry.getElementsByAttributeValue("style", "clear:both").get(0);
         int clearIndex = entry.childNodes().indexOf(clearElement) - 1;
 
         StringBuilder descriptionHtmlBuilder = new StringBuilder();
-        for(int i = clearIndex; i < entry.childrenSize();i++){
+        for (int i = clearIndex; i < entry.childrenSize(); i++) {
             descriptionHtmlBuilder.append(entry.child(i).html());
             descriptionHtmlBuilder.append("<br>");
         }
@@ -41,4 +59,19 @@ public class Event {
         return description;
     }
 
+    public String getDate() {
+        return date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public String getLocationName() {
+        return locationName;
+    }
+
+    public String getLocationLink() {
+        return locationLink;
+    }
 }
--- app/src/main/java/me/robbyblue/rauszeit/EventActivity.java
+++ app/src/main/java/me/robbyblue/rauszeit/EventActivity.java
@@ -1,6 +1,9 @@
 package me.robbyblue.rauszeit;
 
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.SpannableStringBuilder;
+import android.text.style.URLSpan;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,7 +19,19 @@ public class EventActivity extends AppCompatActivity {
 
         RauszeitAPI.getEvent(url, (event) -> {
             ((TextView) findViewById(R.id.eventName)).setText(event.getName());
+            ((TextView) findViewById(R.id.eventDate)).setText(event.getDate());
+            ((TextView) findViewById(R.id.eventTime)).setText(event.getTime());
             ((TextView) findViewById(R.id.eventDescription)).setText(event.getDescription());
+
+            TextView locationView = findViewById(R.id.eventLocation);
+            String locationName = event.getLocationName();
+            SpannableStringBuilder locationSpan = new SpannableStringBuilder(locationName);
+            locationSpan.setSpan(new URLSpan(""), 0, locationName.length(), SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE);
+            locationView.setText(locationSpan);
+
+            locationView.setOnClickListener((e) -> {
+                // TODO: open activity about the location
+            });
         });
     }
 
